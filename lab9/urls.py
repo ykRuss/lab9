c09bd901a72ff76595fa69e5308d43973ef9dc5d
nova <39466571+ykRuss@users.noreply.github.com>
2024-12-02 00:57:12 -0500

    Delete lab9 directory

--- lab9/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from app_name import views 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  
-    path('movies/', include('app_name.urls')),  
-]
